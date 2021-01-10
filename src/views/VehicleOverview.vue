0c67a20481f844b12c6c27178366c23abffe5c38
Dylan Gore <hello@dylangore.ie>
2021-01-10 21:32:21 +0000

    :sparkles: Add interactive graph for vehicle speed

--- src/views/VehicleOverview.vue
+++ src/views/VehicleOverview.vue
@@ -1,51 +1,115 @@
 <template>
   <div class="container-fluid">
-    <div class="col-12">
-      <h1>Vehicle: {{ $route.params.vehicleId }}</h1>
+    <div class="row">
+      <div class="col-12">
+        <h1>Vehicle: {{ $route.params.vehicleId }}</h1>
+      </div>
     </div>
-    <div class="col-md-6 col-sm-12">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Time</th>
-            <th scope="col">Field</th>
-            <th scope="col">Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="entry in data" :key="entry.time">
-            <td>{{ entry.time }}</td>
-            <td>{{ entry.field }}</td>
-            <td>{{ entry.value }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row">
+      <!-- <div class="col-md-6 col-sm-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">Field</th>
+              <th scope="col">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="entry in data" :key="entry.time">
+              <td>{{ parseDate(entry.time) }}</td>
+              <td>{{ entry.field }}</td>
+              <td>{{ entry.value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div> -->
+      <div class="col-md-6 col-sm-12">
+        <div id="chart"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ApexCharts from 'apexcharts';
+import { format } from 'date-fns';
 
 export default defineComponent({
   name: 'VehicleOverview',
   data() {
     return {
-      data: {}
+      data: []
     };
+  },
+  computed: {
+    chartSpeed() {
+      return result;
+    }
   },
   mounted() {
     this.getVehicleData();
+    this.$nextTick(() => {});
   },
   methods: {
     getVehicleData() {
-      this.axios.get('http://localhost:5000/vehicles/${$route.params.vehicleId}/speed').then((response) => {
+      console.log(this.$route.params.vehicleId);
+      this.axios.get(`http://localhost:5000/vehicles/${this.$route.params.vehicleId}/speed`).then((response) => {
         this.data = response.data;
-        console.log(response.data);
+
+        var result = [];
+        this.data.forEach((entry) => {
+          result.push({ x: new Date(entry.time).getTime(), y: entry.value });
+        });
+
+        console.log(result);
+
+        var options = {
+          chart: {
+            type: 'line'
+          },
+          series: [
+            {
+              name: 'Speed',
+              data: result
+            }
+          ],
+          xaxis: {
+            type: 'datetime',
+            min: new Date('2021-01-05T14:30:00').getTime(),
+            max: new Date('2021-01-05T14:40:00').getTime()
+          },
+          yaxis: {
+            labels: {
+              formatter: function (value) {
+                return value + ' km/h';
+              }
+            }
+          },
+          tooltip: {
+            x: {
+              formatter(timestamp) {
+                return format(new Date(timestamp), 'dd MMM y pp');
+              }
+            }
+          },
+          title: {
+            text: 'Speed'
+          },
+          markers: {
+            size: 1
+          }
+        };
+
+        var chart = new ApexCharts(document.querySelector('#chart'), options);
+
+        chart.render();
       });
+    },
+    parseDate(date: string) {
+      return new Date(date);
     }
   }
 });
 </script>
-
-<style></style>
