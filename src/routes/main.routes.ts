729bd33a88e74fb48c40fe21964457d52ca92c12
Dylan Gore <hello@dylangore.ie>
2021-04-23 18:27:09 +0100

    :zap: Switch to dynamic component imports to improve performance

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -1,28 +1,24 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import firebase from 'firebase/app';
-
-// General Components
-// @ts-ignore
-import Dashboard from '../views/Dashboard.vue';
-// @ts-ignore
-import VehicleOverview from '../views/VehicleOverview.vue';
-
-// Auth-Related Components
-// @ts-ignore
-import Login from '../components/auth/Login.vue';
-// @ts-ignore
-import Register from '../components/auth/Register.vue';
-// @ts-ignore
-import ForgotPassword from '../components/auth/ForgotPassword.vue';
+import store from '../stores/auth.store';
 
 const history = createWebHistory();
 const routes = [
   { name: 'home', path: '/', redirect: '/dashboard' },
-  { name: 'login', path: '/login', component: Login },
-  { name: 'register', path: '/register', component: Register },
-  { name: 'forgotPassword', path: '/forgot-password', component: ForgotPassword },
-  { name: 'dashboard', path: '/dashboard', meta: { requiresAuth: true }, component: Dashboard },
-  { name: 'vehicleOverview', path: '/vehicle/:vehicleId', meta: { requiresAuth: true }, component: VehicleOverview }
+  // @ts-ignore
+  { name: 'login', path: '/login', component: () => import('../components/auth/Login.vue') },
+  // @ts-ignore
+  { name: 'register', path: '/register', component: () => import('../components/auth/Register.vue') },
+  // @ts-ignore
+  { name: 'forgotPassword', path: '/forgot-password', component: () => import('../components/auth/ForgotPassword.vue') },
+  // @ts-ignore
+  { name: 'dashboard', path: '/dashboard', meta: { requiresAuth: true }, component: () => import('../views/Dashboard.vue') },
+  // @ts-ignore
+  { name: 'vehicleOverview', path: '/vehicle/:vehicleId', meta: { requiresAuth: true }, component: () => import('../views/VehicleOverview.vue') },
+  // @ts-ignore
+  { name: 'map', path: '/map', meta: { requiresAuth: true }, component: () => import('../components/Map.vue') },
+  // @ts-ignore
+  { name: 'profile', path: '/profile', meta: { requiresAuth: true }, component: () => import('../components/auth/Profile.vue') }
 ];
 
 const router = createRouter({ history, routes });
@@ -31,7 +27,7 @@ router.beforeEach((to, _from, next) => {
   // If the route requires authentication
   if (to.matched.some((rec) => rec.meta.requiresAuth)) {
     // Check the user's authentication state
-    if (firebase.auth().currentUser) {
+    if (store.getters.getUser) {
       // If user is logged in, continue
       next();
     } else {
